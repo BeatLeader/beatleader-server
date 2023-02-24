d5fe6b84a9aca2f15d4b23b385eeb3c77553af78
NSGolova <golova@golova.dev>
2023-02-24 14:40:49 +0000

    Removed OP marking service

--- Startup.cs
+++ Startup.cs
@@ -235,7 +235,6 @@ namespace BeatLeader_Server {
                 services.AddHostedService<HistoryService>();
                 services.AddHostedService<RankingService>();
                 services.AddHostedService<MinuteRefresh>();
-                services.AddHostedService<MarkOPScores>();
             }
             services.AddHostedService<BotService>();
             services.AddSingleton<NominationsForum>();
