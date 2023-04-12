aa523b2a4361a2f66fec98dae9a062fd8864cde7
redageddon <therealrubiksmaster@gmail.com>
2023-04-12 14:58:39 -0700

    added casts for convenient data conversions

--- Services/SearchService/SongService/SongMetadate.cs
+++ Services/SearchService/SongService/SongMetadate.cs
@@ -1,8 +1,9 @@
 ﻿using BeatLeader_Server.Models;
+using Lucene.Net.Documents;
 
 namespace BeatLeader_Server.Services;
 
-internal class SongMetadata
+public class SongMetadata
 {
     public string Id { get; private set; }
 
@@ -23,4 +24,32 @@ internal class SongMetadata
             Author = song.Author.ToLower(),
             Mapper = song.Mapper.ToLower(),
         };
+
+    public static explicit operator SongMetadata(Document doc) =>
+        new()
+        {
+            Id = doc.Get(nameof(Id)),
+            Name = doc.Get(nameof(Name)),
+            Hash = doc.Get(nameof(Hash)),
+            Author = doc.Get(nameof(Author)),
+            Mapper = doc.Get(nameof(Mapper)),
+        };
+
+    public static explicit operator Document(SongMetadata songMetadata)
+    {
+        Document document = new();
+        Field idField = new(nameof(Id), songMetadata.Id, Field.Store.YES, Field.Index.ANALYZED);
+        Field nameField = new(nameof(Name), songMetadata.Name, Field.Store.YES, Field.Index.ANALYZED);
+        Field hashField = new(nameof(Hash), songMetadata.Hash, Field.Store.YES, Field.Index.ANALYZED);
+        Field authorField = new(nameof(Author), songMetadata.Author, Field.Store.YES, Field.Index.ANALYZED);
+        Field mapperField = new(nameof(Mapper), songMetadata.Mapper, Field.Store.YES, Field.Index.ANALYZED);
+
+        document.Add(idField);
+        document.Add(nameField);
+        document.Add(hashField);
+        document.Add(authorField);
+        document.Add(mapperField);
+
+        return document;
+    }
 }
