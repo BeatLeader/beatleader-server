ee5edf2e2a02f6d9d1da86ac3842db2fe473f591
NSGolova <golova@golova.dev>
2023-12-03 22:32:56 +0000

    Added headset stats endpoint

--- Controllers/Admin/PlayerAdminController.cs
+++ Controllers/Admin/PlayerAdminController.cs
@@ -4,6 +4,7 @@ using BeatLeader_Server.Models;
 using BeatLeader_Server.Utils;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Json.Serialization;
 
 namespace BeatLeader_Server.Controllers
 {
@@ -146,6 +147,57 @@ namespace BeatLeader_Server.Controllers
             await _context.SaveChangesAsync();
 
             return Ok();
+        }
+
+        public class HMDStat {
+            public string Headset { get; set; }
+            public string Value { get; set; }
+
+            [JsonIgnore]
+            public float FloatValue { get; set; }
+        }
+
+        [HttpGet("~/admin/headsetStats")]
+        [Authorize]
+        public async Task<ActionResult<List<HMDStat>>> GetHeadsetStats()
+        {
+            string currentId = HttpContext.CurrentUserID(_context);
+            Player? currentPlayer = await _context.Players.FindAsync(currentId);
+            if (currentPlayer == null || !currentPlayer.Role.Contains("admin"))
+            {
+                return Unauthorized();
+            }
+
+            var timeset = (int)DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalSeconds;
+            var activeTreshold = timeset - 60 * 60 * 24 * 31 * 3;
+
+            var scores = _context
+                .Scores
+                .Where(s => s.Timepost > activeTreshold)
+                .Select(s => new {
+                    s.PlayerId,
+                    s.Hmd,
+                    s.Platform
+                })
+                .ToList();
+
+            var keys = scores.DistinctBy(s => s.Hmd.ToString() + "," + s.Platform.Split(",").First()).Select(s => s.Hmd.ToString() + "," + s.Platform.Split(",").First()).ToList();
+            var groups = scores.GroupBy(s => s.PlayerId + s.Hmd + s.Platform.Split(",").First()).ToList();
+
+            var totalCount = groups.Count();
+            var result = new List<HMDStat>();
+            foreach (var key in keys)
+            {
+                float value = ((float)groups.Count(g => g.First().Hmd.ToString() + "," + g.First().Platform.Split(",").First() == key) / totalCount) * 100f;
+
+                result.Add(new HMDStat {
+                    Headset = key,
+                    Value = Math.Round(value, 2) + "%",
+                    FloatValue = value
+                });
+            }
+            
+            return result.OrderByDescending(s => s.FloatValue).ToList();
         }
     }
 }
