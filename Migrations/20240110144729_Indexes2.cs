3317e39a92ee5287a0a2075b8b5dadce5a9f6b20
NSGolova <golova@golova.dev>
2024-01-11 15:30:05 +0000

    More indexes

---
+++ Migrations/20240110144729_Indexes2.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BeatLeader_Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class Indexes2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Scores_Accuracy",
+                table: "Scores",
+                column: "Accuracy");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Scores_Pp",
+                table: "Scores",
+                column: "Pp");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Scores_Timepost",
+                table: "Scores",
+                column: "Timepost");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Scores_Accuracy",
+                table: "Scores");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Scores_Pp",
+                table: "Scores");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Scores_Timepost",
+                table: "Scores");
+        }
+    }
+}
