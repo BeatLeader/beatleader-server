bf776966604429564f44ad9d51fa1d576442fcc7
NSGolova <golova@golova.dev>
2024-02-25 00:20:14 +0000

    Added awards status support

---
+++ Migrations/20240224224909_MapAwardsStatus.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BeatLeader_Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class MapAwardsStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Details",
+                table: "ExternalStatus",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "ExternalStatus",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TitleColor",
+                table: "ExternalStatus",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Details",
+                table: "ExternalStatus");
+
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "ExternalStatus");
+
+            migrationBuilder.DropColumn(
+                name: "TitleColor",
+                table: "ExternalStatus");
+        }
+    }
+}
